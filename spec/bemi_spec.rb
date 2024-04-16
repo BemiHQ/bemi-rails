630a230b66ec3d394e39134d3b6c1290238edd75
exAspArk <exaspark@gmail.com>
2024-04-16 09:14:48 -0400

    Add more specs

--- spec/bemi_spec.rb
+++ spec/bemi_spec.rb
@@ -12,24 +12,56 @@ RSpec.describe Bemi do
   end
 
   describe '.append_context' do
-    it 'appends the context to write SQL queries' do
-      Bemi.set_context({ foo: 'bar' })
-      sql = "INSERT INTO todos (task) VALUES ('Eat')"
-      adapter = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new({})
+    context 'with adapter' do
+      it 'appends the context to write SQL queries' do
+        Bemi.set_context({ foo: 'bar' })
+        sql = "INSERT INTO todos (task) VALUES ('Eat')"
+        adapter = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new({})
 
-      result = Bemi.append_context.call(sql, adapter)
+        result = Bemi.append_context.call(sql, adapter)
 
-      expect(result).to eq("INSERT INTO todos (task) VALUES ('Eat') /*Bemi {\"foo\":\"bar\"} Bemi*/")
+        expect(result).to eq("INSERT INTO todos (task) VALUES ('Eat') /*Bemi {\"foo\":\"bar\"} Bemi*/")
+      end
+
+      it 'does not append the context to read SQL queries' do
+        Bemi.set_context({ foo: 'bar' })
+        sql = "SELECT * FROM todos"
+        adapter = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new({})
+
+        result = Bemi.append_context.call(sql, adapter)
+
+        expect(result).to eq("SELECT * FROM todos")
+      end
+
+      it 'does not append the context if it is too large' do
+        Bemi.set_context({ foo: 'a' * 1_000_001 })
+        sql = "INSERT INTO todos (task) VALUES ('Eat')"
+        adapter = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new({})
+
+        result = Bemi.append_context.call(sql, adapter)
+
+        expect(result).to eq("INSERT INTO todos (task) VALUES ('Eat')")
+      end
     end
 
-    it 'does not append the context to read SQL queries' do
-      Bemi.set_context({ foo: 'bar' })
-      sql = "SELECT * FROM todos"
-      adapter = ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.new({})
+    context 'without adapter with Rails < 7.1' do
+      it 'appends the context to write SQL queries' do
+        Bemi.set_context({ foo: 'bar' })
+        sql = "INSERT INTO todos (task) VALUES ('Eat')"
 
-      result = Bemi.append_context.call(sql, adapter)
+        result = Bemi.append_context.call(sql)
 
-      expect(result).to eq("SELECT * FROM todos")
+        expect(result).to eq("INSERT INTO todos (task) VALUES ('Eat') /*Bemi {\"foo\":\"bar\"} Bemi*/")
+      end
+
+      it 'does not append the context to read SQL queries' do
+        Bemi.set_context({ foo: 'bar' })
+        sql = "SELECT * FROM todos"
+
+        result = Bemi.append_context.call(sql)
+
+        expect(result).to eq("SELECT * FROM todos")
+      end
     end
   end
 end
